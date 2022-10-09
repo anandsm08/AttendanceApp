615790b35fd0e037466e96e60a448456c5d42aca
Tom-Mouse-p <pranavrajeevan04@gmail.com>
2022-10-09 16:46:53 +0530

    Dashboard Finished

--- app/src/main/java/com/example/attendanceapp/MainActivity.kt
+++ app/src/main/java/com/example/attendanceapp/MainActivity.kt
@@ -3,24 +3,25 @@ package com.example.attendanceapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.attendanceapp.R
 import com.example.attendanceapp.ui.theme.AttendanceAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -28,19 +29,221 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             AttendanceAppTheme {
-                MarkedPage()
+//                MarkedPage()
+                Dashboard()
             }
         }
     }
 }
-//fun formatStr(str:String){
-//    if
-//}
 
+val blueColor = "#00BFFF"
 fun getColor(c:String):Color{
     return Color(android.graphics.Color.parseColor(c))
 }
 
+/*
+* Pranav Rajeevan Start
+* */
+@Composable
+fun Dashboard (
+    name: String = "Prof. Shruti Patil",
+    greet: String = "Morning",
+    dayy: String = "Today",
+    dateStr: String = "24/10/2020",
+    timeStr: String = "12:04pm",
+) {
+    Column(
+
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+            .background(getColor("#F0FFFF"))
+            .padding(top = 30.dp)
+            .padding(horizontal = 30.dp)
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.End,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            RoundImage(
+                image = painterResource(id = R.drawable.profile_pic),
+                modifier = Modifier
+                    .size(60.dp)
+            )
+        }
+        Spacer(modifier = Modifier.width(60.dp))
+        Text(text = "Good $greet",
+            color = getColor("#00BFFF"),
+            fontWeight = FontWeight.W700,
+            fontSize = 30.sp,
+            overflow = TextOverflow.Ellipsis
+        )
+        Text(text = name,
+            color = Color.Black,
+            fontWeight = FontWeight.W700,
+            fontSize = 40.sp,
+            overflow = TextOverflow.Ellipsis
+        )
+
+        Spacer(modifier = Modifier.height(15.dp))
+
+        DayDate(
+            dayy, dateStr, timeStr
+        )
+
+        Spacer(modifier = Modifier.height(15.dp))
+
+        Text(text = "Your Classes",
+            color = getColor("#00BFFF"),
+            fontWeight = FontWeight.W700,
+            fontSize = 25.sp,
+            overflow = TextOverflow.Ellipsis,
+            modifier = Modifier
+                .fillMaxWidth()
+            )
+
+            ShowList()
+
+    }
+}
+
+@Composable
+fun RoundImage(
+    image: Painter,
+    modifier: Modifier = Modifier
+) {
+    Image(
+        painter = image,
+        contentDescription = null,
+        modifier = modifier
+            .aspectRatio(1f, matchHeightConstraintsFirst = true)
+            .size(5.dp)
+            .border(
+                width = 1.dp,
+                color = getColor("#00BFFF"),
+                shape = CircleShape
+            )
+            .padding(3.dp)
+            .clip(CircleShape)
+    )
+}
+
+@Composable
+fun DayDate(
+//    modifier: Modifier= Modifier,
+    dayy: String = "Today",
+    dateStr: String = "24/10/2020",
+    timeStr: String = "12:04pm",
+){
+    Text(text = dayy,
+        color = getColor("#708090"),
+        fontWeight = FontWeight.W700,
+        fontSize = 15.sp,
+    )
+    Text(text = dateStr,
+        color = getColor("#708090"),
+        fontWeight = FontWeight.W700,
+        fontSize = 15.sp
+    )
+    Text(text = timeStr,
+        color = getColor("#708090"),
+        fontWeight = FontWeight.W700,
+        fontSize = 15.sp
+    )
+}
+
+@Composable
+fun ShowList(
+    modifier: Modifier = Modifier
+){
+        val sections = listOf("First Year","Second Year", "Third Year")
+        val classs = listOf("SE_IT_A", "SE_IT_B", "SE_COMPS_A")
+
+
+        val scrollstate = rememberScrollState()
+        Card(
+            shape = RoundedCornerShape(topEnd = 12.dp, topStart = 12.dp),
+            modifier = Modifier
+                .fillMaxHeight()
+        ) {
+            Column(
+                modifier = Modifier
+                    .verticalScroll(state = scrollstate)
+                    .background(Color.White)
+                    .padding(15.dp)
+                ,
+                verticalArrangement = Arrangement.spacedBy(15.dp)
+            ) {
+                for (section in sections){
+                    MakeCard(section)
+                }
+            }
+        }
+}
+
+@Composable
+fun MakeCard(
+    section: String,
+){
+    Text(
+        text = section,
+        color = getColor("#00BFFF"),
+        fontWeight = FontWeight.W700,
+        fontSize = 20.sp,
+        overflow = TextOverflow.Ellipsis,
+        modifier = Modifier
+            .fillMaxWidth()
+    )
+
+    for(i in 1..5) {
+        HomeCard(classname = "SE_IT_B")
+    }
+}
+
+@Composable
+fun HomeCard(classname: String){
+    Button(
+        onClick = { /*TODO*/ },
+        colors = ButtonDefaults.buttonColors(
+            backgroundColor = getColor(blueColor)
+        ),
+        shape = RoundedCornerShape(6.dp),
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(horizontal = 6.dp, vertical = 6.dp)
+        ) {
+            Text(text=classname, color = Color.White, fontSize = 15.sp, fontWeight = FontWeight.SemiBold)
+            Row(
+                Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.End
+            ) {
+                Row(
+                    modifier = Modifier.height(30.dp),
+                ) {
+                    RoundImage(
+                        image = painterResource(R.drawable.arrowcircle),
+                        modifier = Modifier
+                            .size(60.dp)
+                    )
+                }
+            }
+        }
+    }
+}
+
+
+//annotation class composable
+
+
+/*
+* Pranav Rajeevan Ends
+* */
+
+/*
+* Pranav Sunil Start
+* */
 @Composable
 fun MarkedPage(){
     Column(horizontalAlignment = Alignment.CenterHorizontally,modifier = Modifier.fillMaxSize()){
@@ -51,9 +254,9 @@ fun MarkedPage(){
 
 @Composable
 fun MarkedHeader(className:String, curTime:String,Day: String, Date: String){
-    Column() {
+    Column {
         Spacer(modifier = Modifier.height(30.dp))
-        Row() {
+        Row {
             Text(text = className, color = getColor("#00BFFF"), fontWeight = FontWeight.W700, fontSize = 20.sp)
             Spacer(modifier = Modifier.width(140.dp))
             Button(
@@ -67,7 +270,7 @@ fun MarkedHeader(className:String, curTime:String,Day: String, Date: String){
         Row(
             verticalAlignment = Alignment.CenterVertically
         ) {
-            Column() {
+            Column {
                 Text(text = Day, color = getColor("#708090"), fontWeight = FontWeight.W600)
                 Text(text = Date, color = getColor("#708090"))
                 Text(text = curTime, color = getColor("#708090"))
@@ -110,10 +313,10 @@ fun MarkedStudentCardContainer(isAbsenteePage: Boolean){
             .width(350.dp)
     ) {
         Column(
-
-            Modifier.fillMaxWidth(),
+            Modifier
+                .fillMaxWidth()
+                .background(getColor("#ffffff")),
             horizontalAlignment = Alignment.CenterHorizontally
-
         ) {
             Row(
                 modifier = Modifier.fillMaxWidth()
@@ -121,8 +324,6 @@ fun MarkedStudentCardContainer(isAbsenteePage: Boolean){
                 Spacer(modifier = Modifier.width(20.dp))
                 PresentAbsentChoiceCard("Present", isAbsenteePage)
                 PresentAbsentChoiceCard("Absent", !isAbsenteePage)
-//                Spacer(modifier = Modifier.width(20.dp))
-
             }
             val scrollState = rememberScrollState()
             Spacer(modifier = Modifier.height(10.dp))
@@ -142,21 +343,21 @@ fun MarkedStudentCardContainer(isAbsenteePage: Boolean){
 
 @Composable
 fun PresentAbsentChoiceCard(str: String, isTransparent: Boolean){
-        Card(
-            backgroundColor = Color.White,
-            shape = RoundedCornerShape(bottomEnd = 12.dp, bottomStart = 12.dp),
-            modifier = Modifier
-                .alpha(if (isTransparent) 0.5F else 1F)
-                .height(50.dp)
+    Card(
+        backgroundColor = Color.White,
+        shape = RoundedCornerShape(bottomEnd = 12.dp, bottomStart = 12.dp),
+        modifier = Modifier
+            .alpha(if (isTransparent) 0.5F else 1F)
+            .height(50.dp)
+    ) {
+        Column(
+            modifier = Modifier.fillMaxHeight(),
+            verticalArrangement = Arrangement.Bottom,
+            horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            Column(
-                modifier = Modifier.fillMaxHeight(),
-                verticalArrangement = Arrangement.Bottom,
-                horizontalAlignment = Alignment.CenterHorizontally,
-            ) {
-                Text(text = str, color = getColor("#00BFFF"), modifier = Modifier.padding(10.dp))
-            }
+            Text(text = str, color = getColor("#00BFFF"), modifier = Modifier.padding(10.dp))
         }
+    }
 }
 
 @Composable
@@ -208,6 +409,9 @@ fun MarkedStudentCard(rollNo: Int, name: String, isPresenteePage: Boolean){
         }
     }
 }
+/*
+* Pranav Sunil End
+* */
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
